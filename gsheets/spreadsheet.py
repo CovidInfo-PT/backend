0458eac0b9c8136578bc67f134c5a9b53ba56add
tiagocmendes <tiagocmendes@ua.pt>
2020-04-06 00:22:22 +0100

    Sheets updated programmatically

--- gsheets/spreadsheet.py
+++ gsheets/spreadsheet.py
@@ -2,15 +2,29 @@ import gspread
 import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 
-sheet = 'https://spreadsheets.google.com/feeds'
+class SpreadSheet:
 
-scope = [sheet,'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client = gspread.authorize(creds)
+    def __init__(self, credentials='credentials.json', name='Validacao_VersaoTestes'):
+        
+        self.scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name(credentials, self.scope)
+        self.client = gspread.authorize(creds)
+        self.sheet = self.client.open(name)
 
-sheet = client.open('Validacao_VersaoTestes').get_worksheet(1)
-pp = pprint.PrettyPrinter()
+    def open_worksheet(self, name='Validacao_VersaoTestes', idx=1):
+        self.worksheet = self.client.open(name).get_worksheet(idx)
+        return self.worksheet
+    
+    def check_cell(self, row, col=24):
+        if row > 0 and col > 0:
+            self.worksheet.update_cell(row, col, 'TRUE')
+    
+    def uncheck_cell(self, row, col=24):
+        if row > 0 and col > 0:
+            self.worksheet.update_cell(row, col, 'FALSE')
+    
+    def write_error(self, row, col=25, error="error"):
+        if row > 0 and col > 0:
+            self.worksheet.update_cell(row, col, error)
+    
 
-print(dir(sheet))
-
-pp.pprint(sheet.get_all_records())
